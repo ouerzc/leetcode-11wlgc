f74d9ceb1353d7321e360a5ac28d6b5ca407426e
Junpeng Shi <junpeng.shi@nexusguard.com>
2019-01-11 18:49:08 +0800

    fix trapping-rain-water jun run.js

--- 2. 接雨水/jun/run.js
+++ 2. 接雨水/jun/run.js
@@ -1,36 +1,29 @@
 /**
- * @param {number[]} heights
+ * @param {number[]} height
  * @return {number}
  */
-var largestRectangleArea = function(heights) {
-    var start = 0;
-    var max = 0;
-    for (let i = 0; i < heights.length; i++) {
-      var area = heights[i] +getAreaLeft(heights, i)+getAreaRight(heights, i)
-      if(area > max) {
-        max = area
-      }
-    }
-    return max
+var trap = function(height) {
+  if(height.length<3){
+    return 0
+  }
+  return find(height,0,height.length-1)
 };
 
-var getAreaLeft = function(heights, start){
-  var index = start - 1
-  var sum = 0
-  while (index>=0 && heights[index] >= heights[start]) {
-    sum += heights[start]
-    -- index
+var find = function(height, start, end) {
+  if(end-start < 2){
+    return 0
   }
-  return sum;
-}
-
-var getAreaRight = function(heights, start){
-  var index = start + 1
-  var length = heights.length
-  var sum = 0
-  while (index<length && heights[index] >= heights[start]) {
-    sum += heights[start]
-    ++ index 
+  let max=-1, tmp=-1, min_two = Math.min(height[start], height[end]), sum=0;
+  for(let i = start+1; i<end; i++){
+    sum = sum+(min_two-height[i]);
+    if(height[i]> max){
+      max=height[i];
+      tmp=i;
+    }
   }
-  return sum;
+  if(max<min_two){
+    return sum
+  }else{
+    return find(height, start, tmp)+find(height,tmp,end)
+  }
 }
